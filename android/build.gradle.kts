30a34a9fab7bf883d494adad0882eda11d876e69
nathanaeru <nathanael.leander@ui.ac.id>
2025-11-23 15:22:44 +0700

    readd internet permission

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,16 +5,19 @@ allprojects {
     }
 }
 
-val newBuildDir: Directory =
-    rootProject.layout.buildDirectory
-        .dir("../../build")
-        .get()
-rootProject.layout.buildDirectory.value(newBuildDir)
+// Comment out or remove these lines:
+// val newBuildDir: Directory =
+//     rootProject.layout.buildDirectory
+//         .dir("../../build")
+//         .get()
+// rootProject.layout.buildDirectory.value(newBuildDir)
 
-subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
+// Comment out or remove these lines:
+// subprojects {
+//     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+//     project.layout.buildDirectory.value(newSubprojectBuildDir)
+// }
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
